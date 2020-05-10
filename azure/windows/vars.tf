dfcfe4ac34a900f26113e110772dc88bbbfec790
Matt Davison <matt45davison@gmail.com>
2020-05-09 22:09:40 -0700

    added variables for configuration of deployment

---
+++ azure/windows/vars.tf
@@ -0,0 +1,69 @@
+variable "resource_group_name" {
+  type = "string"
+  description = "Name of the resource group which will contain all deployments"
+  default = "terraria-RG"
+}
+
+variable "location" {
+  type = "string"
+  description = "Name of the location of the deployment"
+  default = "eastus"
+}
+
+variable "vnet_name" {
+  type = "string"
+  description = "Name of the virtual network"
+  default = "terraria-VNET"
+}
+
+variable "subnet_name" {
+  type = "string"
+  description = "Name of the sub network"
+  default = "terraria-SUBNET"
+}
+
+variable "vm_name" {
+  type = "string"
+  description = "Name of the terraria server"
+  default = "terraria-WINDOWS"
+}
+
+variable "vm_size" {
+  type = "string"
+  description = "Size of the virtual machine used to deploy the terraria server (default meets minimum requirements to host large worl)"
+  default = "Standard_B1ms"
+}
+
+variable "vm_admin_name" {
+  type = "string"
+  description = "Login name of the admin user for the virtual machine"
+  default = "adminTerraria"
+}
+
+variable "vm_admin_password" {
+  type = "string"
+  description = "Password of the admin user for the virtual machine"
+  default = "sTroNg1357pAsSwOrD"
+}
+
+variable "nic_name" {
+  type = "string"
+  description = "Name of the network integrated card"
+  default = "terraria-NIC"
+}
+
+variable "public_ip_name" {
+  type = "string"
+  description = "Name of the public ip address"
+  default = "terraria-public-IP"
+}
+
+
+
+
+
+
+
+
+
+
